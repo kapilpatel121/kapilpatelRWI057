80f2bfb93eadbb203202cb7d1146e88365e76153
kapilpatel121 <163231457+kapilpatel121@users.noreply.github.com>
2024-04-03 12:58:04 +0530

    Delete Billing-system directory

--- Billing-system/src/main/java/com/uc/repo/InvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uc.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.uc.entity.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
--- Billing-system/src/main/java/com/uc/repo/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uc.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.uc.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
-
